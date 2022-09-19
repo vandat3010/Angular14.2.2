d77e5765d713bb8dba911775904151895dc1fa7b
vandat3010 <vandat1095@gmail.com>
2022-09-19 22:10:21 +0700

    properties and *ngIf

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  public name ='Huy';
+  public name ='Đạt Ngô';
+  public age = 15;
   constructor() { }
 
   ngOnInit(): void {
