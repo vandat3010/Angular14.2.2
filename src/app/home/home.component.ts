4dfc4c3d08cff5b2ed1b833ce25794475642c843
vandat3010 <vandat1095@gmail.com>
2022-09-19 22:55:53 +0700

    V3

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public name ='Đạt Ngô';
+  public age = 25;
   constructor() { }
 
   ngOnInit(): void {
