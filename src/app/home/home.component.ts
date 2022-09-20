42a5df5aa97fcb4ca56aa9737b4b8b374b9e5c29
vandat3010 <vandat1095@gmail.com>
2022-09-20 22:38:48 +0700

    Pipe định dạng kiểu như Ngày tháng tiền viết hoa viết thường phần trăm...

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,7 @@ export class HomeComponent implements OnInit {
   public name ='Đạt Ngô';
   public age = 25;
   public traiCay = ['Táo','Nho', 'Cam', 'Quýt'];
-  public traiCay2= [{ ten: 'Táo', gia: 12}, {ten: 'Cam', gia: -14}, {ten: 'Quýt', gia: 12}];
+  public traiCay2= [{ ten: 'Táo', gia: 12, FlashSale: 20}, {ten: 'Cam', gia: 14, FlashSale: 40}, {ten: 'Quýt', gia: 12}];
   constructor() { }
 
   public ngOnInit(): void {
---
+++ src/app/on-sale.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'onSale'
+})
+export class OnSalePipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): string {
+    if (value){
+      return 'Siêu giảm giá !';
+    }
+    return '';
+  }
+
+}
