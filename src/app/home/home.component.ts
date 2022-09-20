ea879c4822e672ab13fe87b637eaf22495cb2778
vandat3010 <vandat1095@gmail.com>
2022-09-20 23:46:07 +0700

    Combox

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,18 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  public name ='Đạt Ngô';
+  public name = 'Đạt Ngô';
   public age = 25;
-  public traiCay = ['Táo','Nho', 'Cam', 'Quýt'];
-  public traiCay2= [{ ten: 'Táo', gia: 12, FlashSale: 20}, {ten: 'Cam', gia: 14, FlashSale: 40}, {ten: 'Quýt', gia: 12}];
+  public traiCay = ['Táo', 'Nho', 'Cam', 'Quýt'];
+  public traiCay2 = [{ ten: 'Táo', gia: 12, FlashSale: 20 }, { ten: 'Cam', gia: 14, FlashSale: 40 }, { ten: 'Quýt', gia: 12 }];
+public districts: string[] = [];
+  public cities = [
+    { city: 'An Giang', district: ['Thành phố Long Xuyên', 'Thành phố Châu Đốc', 'Thị xã Tân Châu', 'Huyện An Phú', 'Huyện Châu Phú', 'Huyện Châu Thành', 'Huyện Chợ Mới', 'Huyện Phú Tân', 'Huyện Thoại Sơn', 'Huyện Tịnh Biên', 'Huyện Tri Tôn'] }
+  ]
   constructor() { }
 
   public ngOnInit(): void {
-    console.log('Trái cây =', this.traiCay)
+    console.log('cities = ', this.cities);
   }
-  public resetName(): void{
-    console.log("resetName");
-    this.name = '';
+  public changeCity(event: any): void{
+    const city = event.target.value;
+    console.log('event', city);
+    if(!city){
+      return;
+    }
+    // //cách 1
+    // const search = this.cities.filter(data => data.city);
+    // console.log('search', search);
+
+    // if(search && search.length > 0){
+    //   this.districts = search[0].district;
+    // }
+    //cách 2
+    this.districts = this.cities.find(data => data.city === city)?.district||[];
   }
 
 }
